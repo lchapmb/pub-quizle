0b50a485dde9342525fb3855ac4c459fedb30311
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-29 16:31:12 +0100

    adds in textfields

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,10 +1,119 @@
-// import for mui
+// import components
 import Nav from "../components/Nav";
 
+// import for mui
+import { Container, Box, TextField } from "@mui/material";
+
 export default function Landing() {
+  const word = "hello";
+  const wordArr = word.split("");
+  const wordLen = wordArr.length;
+
   return (
     <>
       <Nav />
+      <Container>
+        <Box
+          component="form"
+          sx={{
+            m: 1,
+            display: "flex",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {wordArr.map((letter) => (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label={letter}
+              sx={{
+                m: 1,
+              }}
+            />
+          ))}
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            m: 1,
+            display: "flex",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {wordArr.map((letter) => (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label={letter}
+              sx={{
+                m: 1,
+              }}
+            />
+          ))}
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            m: 1,
+            display: "flex",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {wordArr.map((letter) => (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label={letter}
+              sx={{
+                m: 1,
+              }}
+            />
+          ))}
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            m: 1,
+            display: "flex",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {wordArr.map((letter) => (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label={letter}
+              sx={{
+                m: 1,
+              }}
+            />
+          ))}
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            m: 1,
+            display: "flex",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {wordArr.map((letter) => (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label={letter}
+              sx={{
+                m: 1,
+              }}
+            />
+          ))}
+        </Box>
+      </Container>
     </>
   );
 }
