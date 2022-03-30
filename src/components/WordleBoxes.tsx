0f45af45a20b838b52f364723a2f765d62fc2d30
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-30 10:59:43 +0100

    textfields now in own component

---
+++ src/components/WordleBoxes.tsx
@@ -0,0 +1,34 @@
+import { Container, Box, TextField } from "@mui/material";
+
+export default function WordleBoxes() {
+  const word = "abcdefg";
+  const wordArr = word.split("");
+
+  return (
+    <Container>
+      {wordArr.map(() => (
+        <Box
+          component="form"
+          sx={{
+            m: 0.5,
+            display: "flex",
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {wordArr.map((letter) => (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label={letter}
+              sx={{
+                m: 0.5,
+              }}
+              size="small"
+            />
+          ))}
+        </Box>
+      ))}
+    </Container>
+  );
+}
