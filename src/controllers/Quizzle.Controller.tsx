2948efea8af6059acd5c8c014f64e7d54737dbfc
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-31 16:28:42 +0100

    puts gists back in

--- src/controllers/Quizzle.Controller.tsx
+++ src/controllers/Quizzle.Controller.tsx
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export abstract class QuizzleController {
   static generateQuizzleWord = async () => {
-    // const res = await axios.get(
-    //   `https://api.github.com/gists/fe2b58729defad2ace4d70957d221dcb`
-    // );
-    // const clue = res.data.files.wordleClue.content;
-    return "pints";
+    const res = await axios.get(
+      `https://api.github.com/gists/fe2b58729defad2ace4d70957d221dcb`
+    );
+    const clue = res.data.files.wordleClue.content;
+    return clue;
   };
 
   static generatePubName = async () => {
-    //   const res = await axios.get(
-    //     `https://api.github.com/gists/7e702d2d3cc93feef867b1b258aa01f2`
-    //   );
-    //   const pub = res.data.files.pub.content;
-    return "Costello's";
+    const res = await axios.get(
+      `https://api.github.com/gists/7e702d2d3cc93feef867b1b258aa01f2`
+    );
+    const pub = res.data.files.pub.content;
+    return pub;
   };
 
   static checkIfStringIsWord = async (word: string) => {
